88867d1ca7e31fbecb0f9739ff2c34bac522fa74
viktoriazakatsyura <109316794+viktoriazakatsyura@users.noreply.github.com>
2023-04-29 10:04:38 +0000

    Deploying to gh-pages from @ viktoriazakatsyura/goit-react-hw-08-phonebook@787fd831e38eec294febacdece139594daf18da8 🚀

---
+++ static/js/266.d077dead.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[266],{1266:function(e,t,r){r.r(t),r.d(t,{default:function(){return c}});var n=r(890),a=r(184);function c(){return(0,a.jsx)(a.Fragment,{children:(0,a.jsx)(n.Z,{variant:"h3",gutterBottom:!0,align:"center",sx:{marginTop:"200px"},color:"lime",children:"WELCOME TO THE PHONE BOOK !!!"})})}}}]);
+//# sourceMappingURL=266.d077dead.chunk.js.map
